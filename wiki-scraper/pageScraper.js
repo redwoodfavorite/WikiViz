760b3f6a587484b94135e8e6ccd95e4f299043f8
redwoodfavorite <joseph.michael.sample@gmail.com>
2014-02-24 15:48:12 -0800

    worked on pagescraper

--- wiki-scraper/pageScraper.js
+++ wiki-scraper/pageScraper.js
@@ -27,22 +27,31 @@ var connection = mysql.createConnection({
   password: ''
 });
 
+Promise.promisifyAll(connection);
+
+connection.queryAsync('USE wikiUrls', function(err) {
+  if(err) {
+    console.log(err);
+  }
+});
+
 //////////////////////////////////////////////////////////////////
 // HELPER FUNCTIONS
 //////////////////////////////////////////////////////////////////
 
+//not using this quite yet
 var createHash = function(url){
     var hash = 0;
     if (url.length == 0) return hash;
     for (i = 0; i < url.length; i++) {
         char = url.charCodeAt(i);
         hash = ((hash<<5)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
+        hash = hash & hash;
     }
     return hash;
 }
 
-
+//returns a list of all local urls
 var getUrlsOnPage = function(html){
   var pageUrls = [];
   var $ = cheerio.load(html);
@@ -59,30 +68,48 @@ var getUrlsOnPage = function(html){
   return pageUrls;
 }
 
+//returns an array of urls that have not yet been added to the database
 var backLinkChecking = function(arrOfUrls){
-  console.log(_.filter(arrOfUrls, function(url){
-    connection.query('SELECT * FROM urls WHERE url = "' + url + '"')
-  }));
+  return Promise.filter(arrOfUrls, function(url){
+    return connection.queryAsync('SELECT * FROM urls WHERE url = "' + url + '"').then(function(result){
+      console.log('Querying database...');
+      return !result[0][0];
+    });
+  }).then(function(arr){console.log('DONE FIRST')})
+  .error(function(err){console.log(err)});
 }
 
+//performs requests for each url in a list and stores each url in database
 var exploreUrls = function(urls){
   return Promise.all(_.map(urls, function(url){
     return request(url)
     .then(function(resp){
-      urlConnections[url] = getUrlsOnPage(resp);
-    });
-  }));
-};
-
+      var pageUrls = getUrlsOnPage(resp);
+      urlConnections[url] = pageUrls;
+      return pageUrls;
+    }).then(function(allUrls){exploreUrls(backLinkChecking(allUrls))});
+  }))
+}
 
 //////////////////////////////////////////////////////////////////
 // INITIATING SEARCH PROCESS
 //////////////////////////////////////////////////////////////////
 
 fs.readFileAsync(path.join(__dirname, 'testFile.txt'), 'utf8')
-  .then(function(contents){ return contents.split(',') })
-  .then(function(urls){
+  .then(function(contents){
+    return contents.split(',');
+  }).then(function(urls){
     return exploreUrls(urls);
-  }).then(console.log(totalPagesVisited + " pages visited!"));
+  }).then(function(){
+    console.log("DONE");
+  }).error(function(err){
+    console.log(err);
+  });
 
+// fs.readFileAsync(path.join(__dirname, 'testFile.txt'), 'utf8')
+//   .then(function(contents){
+//     return contents.split(',');
+//   }).then(function(urls){
+//     return backLinkChecking(urls);
+//   }).then(function(){console.log('DONE SECOND')});
 
